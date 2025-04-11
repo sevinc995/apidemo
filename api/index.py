002ae614c14169525ccc0d2bb1f9fd6d01fc3510
sevinc995 <sevincseferova25@gmail.com>
2025-04-11 19:07:26 +0000

    data

---
+++ api/index.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from  .config import API_KEY
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route("/api/news", methods=["GET"])
+def get_news():
+
+    api_key = request.args.get("api_key")
+
+    news = { "id": 1, "title": "Demo"}
+
+    if api_key and api_key != API_KEY:
+        return jsonify({"Error": "Invalid api key"})
+    return jsonify({"news": news})
+
