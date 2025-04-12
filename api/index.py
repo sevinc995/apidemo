1cfc91af339a92d3a6c67d129831d97f3bb140e9
sevinc995 <sevincseferova25@gmail.com>
2025-04-12 07:17:59 +0000

    vercel

--- api/index.py
+++ api/index.py
@@ -4,7 +4,10 @@ from  .config import API_KEY
 
 app = Flask(__name__)
 
-CORS(app)
+CORS(app, resources = {r"/api/news":{
+    "origins":[].
+    "methods": [GET]
+}})
 
 @app.route("/api/news", methods=["GET"])
 def get_news():
@@ -12,6 +15,10 @@ def get_news():
     api_key = request.args.get("api_key")
 
     news = { "id": 1, "title": "Demo"}
+
+    if api_key is None and len(requesr.args)>0:
+
+        return jsonify({"error": "Invalid query parameters"})
 
     if api_key and api_key != API_KEY:
         return jsonify({"Error": "Invalid api key"})
